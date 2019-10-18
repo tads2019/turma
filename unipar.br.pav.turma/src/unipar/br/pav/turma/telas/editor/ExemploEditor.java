e7a7413adadee035dd6358884714889dc0e95dcf
Filipe André Wutzke <filipewutzke@gmail.com>
2019-10-18 15:27:38 -0300

    Colocando binding no exemplo

--- unipar.br.pav.turma/src/unipar/br/pav/turma/telas/editor/ExemploEditor.java
+++ unipar.br.pav.turma/src/unipar/br/pav/turma/telas/editor/ExemploEditor.java
@@ -1,27 +1,35 @@
 package unipar.br.pav.turma.telas.editor;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
+import unipar.br.pav.turma.modelo.Exemplo;
 import unipar.br.pav.turma.telas.editor.editorInput.ExemploEditorInput;
-
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.layout.GridData;
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.jface.databinding.swt.WidgetProperties;
+import org.eclipse.core.databinding.beans.PojoProperties;
 
 public class ExemploEditor extends AbstractEditor {
+	private DataBindingContext bindingContext;
 
 	public static final String ID = "unipar.br.pav.turma.telas.editor.ExemploEditor"; //$NON-NLS-1$
 	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
+	private Exemplo exemplo;
+	
 	private Text txtTexto;
 
 	public ExemploEditor() {
+		
 	}
 
 	@Override
@@ -37,6 +45,8 @@ public class ExemploEditor extends AbstractEditor {
 		txtTexto = new Text(compositeTop, SWT.BORDER);
 		txtTexto.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		formToolkit.adapt(txtTexto, true, true);
+		
+		bindingContext = initDataBindings();
 	}
 
 	@Override
@@ -65,8 +75,18 @@ public class ExemploEditor extends AbstractEditor {
 	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
 		// Initialize the editor part
 		ExemploEditorInput exemploEditorInput = (ExemploEditorInput) input;
+		this.exemplo = new Exemplo();
 		setSite(site);
 		setInput(exemploEditorInput);
 	}
 
+	protected DataBindingContext initDataBindings() {
+		DataBindingContext bindingContext = new DataBindingContext();
+		//
+		IObservableValue observeTextTxtTextoObserveWidget = WidgetProperties.text(SWT.Modify).observe(txtTexto);
+		IObservableValue exemploExemploObserveValue = PojoProperties.value("exemplo").observe(exemplo);
+		bindingContext.bindValue(observeTextTxtTextoObserveWidget, exemploExemploObserveValue, null, null);
+		//
+		return bindingContext;
+	}
 }
